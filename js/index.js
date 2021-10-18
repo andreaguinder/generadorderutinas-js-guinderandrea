cf7482b41ee8512d98fe1923814feb7dc2831030
andreaguinder <andreabelen.guinder@gmail.com>
2021-10-17 21:20:18 -0300

    mejoras

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@ $("#btn-generarRutina").append(`<div>
 $("#carrouselEspacioGym").append(`<h2 class="text-center m-2 align-content-center">Conocé nuestro espacio</h2>
                                     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                                     <div class="carousel-inner">
-                                    <div class="carousel-item active"  style="height:60vh" >
-                                        <img src="images/pexels-photo-1954524.jpeg" class="d-block w-100" alt="...">
+                                    <div class="carousel-item active"  style="height:80vh" >
+                                        <img src="./images/carrousel-1.jpg" class="d-block w-100" alt="...">
                                     </div>
-                                    <div class="carousel-item"  style="height:60vh" >
-                                        <img src="images/24-hours-gym-r0lg4mkl4m.jpeg" class="d-block w-100" alt="...">
+                                    <div class="carousel-item"  style="height:80vh" >
+                                        <img src="./images/24-hours-gym-r0lg4mkl4m.jpeg" class="d-block w-100" alt="...">
                                     </div>
-                                    <div class="carousel-item"  style="height:60vh" >
-                                        <img src="images/fitness-center_p.jpg" class="d-block w-100"alt="...">
+                                    <div class="carousel-item"  style="height:80vh" >
+                                        <img src="./images/fitness-center_p.jpg" class="d-block w-100"alt="...">
                                     </div>
                                     </div>
                                 </h1>`
