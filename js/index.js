db13d18e943fced6856846a5938c35aee4aa473d
andreaguinder <andreabelen.guinder@gmail.com>
2021-10-17 21:13:53 -0300

    mod enlaces fotos

--- js/index.js
+++ js/index.js
@@ -10,13 +10,13 @@ $("#carrouselEspacioGym").append(`<h2 class="text-center m-2 align-content-cente
                                     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                                     <div class="carousel-inner">
                                     <div class="carousel-item active"  style="height:60vh" >
-                                        <img src="/images/pexels-photo-1954524.jpeg" class="d-block w-100" alt="...">
+                                        <img src="images/pexels-photo-1954524.jpeg" class="d-block w-100" alt="...">
                                     </div>
                                     <div class="carousel-item"  style="height:60vh" >
-                                        <img src="/images/24-hours-gym-r0lg4mkl4m.jpeg" class="d-block w-100" alt="...">
+                                        <img src="images/24-hours-gym-r0lg4mkl4m.jpeg" class="d-block w-100" alt="...">
                                     </div>
                                     <div class="carousel-item"  style="height:60vh" >
-                                        <img src="/images/fitness-center_p.jpg" class="d-block w-100"alt="...">
+                                        <img src="images/fitness-center_p.jpg" class="d-block w-100"alt="...">
                                     </div>
                                     </div>
                                 </h1>`
