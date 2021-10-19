9c59bfba5e0c0bc18bf8ba8dc969c24565fdc3df
andreaguinder <andreabelen.guinder@gmail.com>
2021-10-19 02:22:03 -0300

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+//hago una peticion de tipo get con jquery
+
+// $.ajax para hacer peticiones en general
+
+/*
+//el primer url es la web a la que quiero llamar, a la que le hago la peticion
+//el segundo es decirle al codigo lo qu evoy a hacer cuando me devuelva la informacion el servidor
+// segundo parametro de callback, es una funcion
+//son funciones que esperan a que termine otra funcion para iniciar las callback
+$.get(url, param2)
+
+//      url       funcion de callbak
+//la respuesta es lo que me devuelve el backend
+//el sttate no es necesario ponerlo. es en numeros en general, por ejemplo el 404
+$.get("https", (response, state) => {
+
+})
+
+$.get("https", ()
+*/
